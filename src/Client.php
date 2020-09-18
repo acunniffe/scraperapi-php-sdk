640b8931807e202f43b9d6cba16d472a14c0ba08
Dusan Stojanovic <dusan.stojanovic@live.com>
2020-09-18 13:04:43 +0200

    Fix code style

--- src/Client.php
+++ src/Client.php
@@ -1,126 +1,155 @@
 <?php
-namespace ScraperAPI {
+namespace ScraperAPI;
 
-    use Unirest\Method;
-    use Unirest\Request;
+use Exception;
+use Unirest\Method;
+use Unirest\Request;
 
-  class Client
-  {
-    public $api_key;
+/**
+ * Class Client
+ * @package ScraperAPI
+ */
+class Client
+{
+    public $apiKey;
 
+    /**
+     * Client constructor.
+     * @param string $key
+     */
     public function __construct($key)
     {
-      $this->api_key = $key;
+        $this->apiKey = $key;
     }
 
-    public function get($url,
-                        $options = [])
+    /**
+     * @param string $url
+     * @param array $options
+     * @return mixed
+     * @throws Exception
+     */
+    public function get($url, $options = [])
     {
-      return $this->scrape($url, Method::GET, $options, null);
+        return $this->scrape($url, Method::GET, $options, null);
     }
 
-    public function post($url,
-                         $options = [],
-                         $body)
+    /**
+     * @param $url
+     * @param array $options
+     * @param $body
+     * @return mixed
+     * @throws Exception
+     */
+    public function post($url, $options = [], $body)
     {
-      return $this->scrape($url, Method::POST, $options, $body);
+        return $this->scrape($url, Method::POST, $options, $body);
     }
 
-    public function put($url,
-                        $options = [],
-                        $body)
+    /**
+     * @param $url
+     * @param array $options
+     * @param $body
+     * @return mixed
+     * @throws Exception
+     */
+    public function put($url, $options = [], $body)
     {
-      return $this->scrape($url, Method::PUT, $options, $body);
+        return $this->scrape($url, Method::PUT, $options, $body);
     }
 
+    /**
+     * @return mixed
+     */
     public function account()
     {
-      $response = Request::get('https://api.scraperapi.com/account', [], ["api_key" => $this->api_key]);
-      return $response->body;
+        $response = Request::get('https://api.scraperapi.com/account', [], ["api_key" => $this->apiKey]);
+
+        return $response->body;
     }
 
-
-    private function scrape($url,
-                            $method,
-                            $options = [],
-                            $body)
+    /**
+     * @param $url
+     * @param $method
+     * @param array $options
+     * @param $body
+     * @return mixed
+     * @throws Exception
+     */
+    private function scrape($url, $method, $options = [], $body)
     {
+        $headers = $options['headers'] ?: [];
+        $countryCode = $options['country_code'];
+        $deviceType = $options['device_type'];
+        $premium = $options['premium'] ?: false;
+        $render = $options['render'] ?: false;
+        $sessionNumber = $options['session_number'];
+        $autoparse = $options['autoparse'] ?: false;
+        $retry = $options['retry'] ?: 3;
+        $timeout = $options['timeout'] ?: 60;
 
+        $query = [
+            'country_code' => $countryCode,
+            "$deviceType" => $deviceType,
+            'api_key' => $this->apiKey,
+            'premium' => $premium,
+            'render' => $render,
+            'session_number' => $sessionNumber,
+            'autoparse' => $autoparse,
+            'keep_headers' => count($headers) > 0,
+            'url' => $url,
+            'scraper_sdk' => 'php'
+        ];
 
-      $headers = $options['headers'] ?: [];
-      $country_code = $options['country_code'];
-      $device_type = $options['device_type'];
-      $premium = $options['premium'] ?: false;
-      $render = $options['render'] ?: false;
-      $session_number = $options['session_number'];
-      $autoparse = $options['autoparse'] ?: false;
-      $retry = $options['retry'] ?: 3;
-      $timeout = $options['timeout'] ?: 60;
+        $filteredQuery = array_filter(
+            $query,
+            static function ($val, $key) {
+                if (isset($val) && is_bool($val)) {
+                    if ($val) {
+                        return $val;
+                    }
+                } else if (isset($val)) {
+                    return $val;
+                }
+            },
+            ARRAY_FILTER_USE_BOTH
+        );
 
-      $query = array(
-        "country_code" => $country_code,
-        "$device_type" => $device_type,
-        "api_key" => $this->api_key,
-        "premium" => $premium,
-        "render" => $render,
-        "session_number" => $session_number,
-        "autoparse" => $autoparse,
-        "keep_headers" => count($headers) > 0,
-        "url" => $url,
-        "scraper_sdk" => "php"
-      );
+        Request::timeout($timeout);
 
-      $filteredQuery = array_filter(
-        $query,
-        function ($val, $key) {
-          if (isset($val) && is_bool($val)) {
-            if ($val) {
-              return $val;
+        $queryString = http_build_query($filteredQuery);
+        $scraperUrl = 'http://api.scraperapi.com/?' . $queryString;
+
+        $makeRequest = function () use ($headers, $body, $scraperUrl, $method) {
+            return Request::send($method, $scraperUrl, $body, $headers);
+        };
+
+        return $this->retryRequest(0, $makeRequest, $retry);
+    }
+
+    /**
+     * @param $try
+     * @param $makeRequest
+     * @param int $max
+     * @return mixed
+     * @throws Exception
+     */
+    private function retryRequest($try, $makeRequest, $max = 1)
+    {
+        $response = null;
+        try {
+            $response = $makeRequest();
+            if ($response->code >= 500) {
+                throw new Exception('Could not reach ScraperAPI Proxy');
             }
-          } else if (isset($val)) {
-            return $val;
-          };
-        },
-        ARRAY_FILTER_USE_BOTH
-      );
+        } catch (Exception $e) {
+            if ($try < $max) {
+                sleep(1);
+                $response = $this->retryRequest($try + 1, $makeRequest, $max);
+            } else {
+                throw new Exception('Failed to connect after ' . $max . ' attempts');
+            }
+        }
 
-      Request::timeout($timeout);
-
-      $queryString = http_build_query($filteredQuery);
-      $scraperUrl = "http://api.scraperapi.com/?" . $queryString;
-
-      $makeRequest = function () use ($headers, $body, $scraperUrl, $method) {
-        return Request::send($method, $scraperUrl, $body, $headers);
-      };
-
-      return retryRequest(0, $makeRequest, $retry);
+        return $response;
     }
-
-
-  }
-
-  function retryRequest($try, $makeRequest, $max = 1)
-  {
-    $response = null;
-    try {
-      $response = $makeRequest();
-      if ($response->code >= 500) {
-        throw new Exception('Could not reach ScraperAPI Proxy');
-      }
-    } catch (Exception $e) {
-      if ($try < $max) {
-        sleep(1);
-        $response = retryRequest($try + 1, $makeRequest, $max);
-      } else {
-        throw new Exception('Failed to connect after ' . $max . ' attempts');
-      }
-    }
-
-    return $response;
-  }
-//  $client = new Client("56d4e886e80d1a3ab41c323a216b16d4");
-//$result = $client->get("http://httpbin.org/ip");
-//var_dump($result->raw_body);
-
 }
-
